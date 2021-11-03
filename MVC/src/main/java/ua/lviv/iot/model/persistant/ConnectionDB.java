e0efe352eaa73fd6628fa87942ff7a2c1a6eefe3
Meksich <maks10031003@gmail.com>
2021-11-03 22:53:02 +0200

    Add connection to DB, entity trasformer and entity manager

--- MVC/src/main/java/ua/lviv/iot/model/persistant/ConnectionDB.java
+++ MVC/src/main/java/ua/lviv/iot/model/persistant/ConnectionDB.java
@@ -1,4 +1,27 @@
 package ua.lviv.iot.model.persistant;
 
+import lombok.NoArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@NoArgsConstructor
 public class ConnectionDB {
+    private static final String URL = "jdbc:mysql://localhost:3306/patients_data?allowPublicKeyRetrieval=true&" +
+            "createDatabaseIfNotExist=true&useSSL=false&serverTimezone=UTC";
+    private static String USER = "root";
+    private static String PASSWORD = "01012015";
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            } catch (SQLException e) {
+                System.out.println("SQLException: " + e.getMessage());
+            }
+        }
+        return connection;
+    }
 }
